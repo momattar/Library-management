6d4e96db21ab67ede76b803624cbb85876146c15
momattar <UG_31093825@f-eng.tanta.edu.eg>
2024-11-22 22:10:10 +0200

    Add project files.

---
+++ myshop.Web/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using myshop.Entities.Models;
+using myshop.Entities.Repositories;
+using myshop.Entities.ViewModels;
+using myshop.Utilities;
+using System.Security.Claims;
+using X.PagedList;
+
+namespace myshop.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly IUnitOfWork _unitofwork; //بعرف ال IUnitOfWork
+
+        public HomeController(IUnitOfWork unitofwork) //Inject IUnitOfWork
+        {
+            _unitofwork = unitofwork;
+        }
+        public IActionResult Index(int ? page)
+        {
+            var PageNumber = page ?? 1;
+            int PageSize = 8;
+
+
+            var products = _unitofwork.Product.GetAll().ToPagedList(PageNumber,PageSize); //To View All the Products From Database
+            return View(products);
+        }
+
+        public IActionResult Details(int ProductId)
+        {
+            ShoppingCart obj = new ShoppingCart()
+            {
+                ProductId = ProductId,
+                Product = _unitofwork.Product.GetFirstorDefault(v => v.Id == ProductId, Includeword: "Category"), //Get the category to present the details
+                Count = 1 
+            };
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            shoppingCart.ApplicationUserId = claim.Value;
+
+            ShoppingCart Cartobj = _unitofwork.ShoppingCart.GetFirstorDefault(
+                u => u.ApplicationUserId == claim.Value && u.ProductId == shoppingCart.ProductId);
+
+            if (Cartobj == null)
+            {
+                _unitofwork.ShoppingCart.Add(shoppingCart);
+                _unitofwork.Complete();
+                HttpContext.Session.SetInt32(SD.SessionKey,
+                    _unitofwork.ShoppingCart.GetAll(x=>x.ApplicationUserId == claim.Value).ToList().Count()
+                   );
+                
+            }
+            else
+            {
+                _unitofwork.ShoppingCart.IncreaseCount(Cartobj, shoppingCart.Count);
+                _unitofwork.Complete();
+            }
+            
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
